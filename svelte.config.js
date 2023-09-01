6c462f0d39e4797a931d8a0cab7793dade4175a8
arvin <arvin_j4848@protonmail.com>
2023-09-01 23:23:15 +0530

    fix:static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
- 
-const dev = process.argv.includes('dev');
- 
+
 /** @type {import('@sveltejs/kit').Config} */
+import preprocess from 'svelte-preprocess';
+const dev = process.argv.includes('dev');
 const config = {
+        preprocess: preprocess(),
   kit: {
     adapter: adapter(),
     paths: {
       base: dev ? '' : process.env.BASE_PATH,
     }
-  }
+  },
 };
+
+export default config; 
